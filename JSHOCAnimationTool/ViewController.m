e7758814a9bb2c054f0fe5460ae891590a042960
JSH <mulan@MuLans-MacBook-Pro.local>
2024-05-22 21:22:33 +0800

    update Bezier

--- JSHOCAnimationTool/ViewController.m
+++ JSHOCAnimationTool/ViewController.m
@@ -5,15 +5,17 @@
 //  Created by JSH on 2024/5/22.
 //
 
+// ViewController.m
+
 #import "ViewController.h"
 #import "HHOIconFlowView.h"
 
 @interface ViewController ()
-@property (nonatomic, strong) UIBezierPath *path;
-@property (nonatomic, strong) UIBezierPath *path1;
-@property (nonatomic, strong) UIBezierPath *path2;
-@property (nonatomic, strong) CAShapeLayer *shapeLayer;
 @property (nonatomic, strong) HHOIconFlowView *iconFlowView;
+
+// 为每种曲线定义一个CAShapeLayer
+@property (nonatomic, strong) CAShapeLayer *quadCurveLayer;
+@property (nonatomic, strong) CAShapeLayer *cubicCurveLayer;
 @end
 
 @implementation ViewController
@@ -25,68 +27,62 @@
 
 - (void)setupUI {
     self.view.backgroundColor = [UIColor whiteColor];
-    self.path = [UIBezierPath bezierPath];
-    self.path1 = [UIBezierPath bezierPath];
-    self.path2 = [UIBezierPath bezierPath];
-    self.shapeLayer = [CAShapeLayer layer];
     
-    // 设置路径属性
-    self.shapeLayer.path = self.path.CGPath;
-    self.shapeLayer.fillColor = [UIColor clearColor].CGColor;
-    self.shapeLayer.strokeColor = [UIColor redColor].CGColor;
-    self.shapeLayer.lineWidth = 2.0;
-    
-    // 添加到视图的layer中
-    [self.view.layer addSublayer:self.shapeLayer];
+    // 初始化曲线层
+    [self setupCurveLayers];
     
     self.iconFlowView = [[HHOIconFlowView alloc] init];
     self.iconFlowView.frame = CGRectMake(15, 15, kLiveLikeFlowSizeW, kLiveLikeFlowSizeH);
     [self.view addSubview:self.iconFlowView];
-   
+}
+
+- (void)setupCurveLayers {
+    // 初始化和配置每个曲线的CAShapeLayer
+    self.quadCurveLayer = [self createShapeLayerWithStrokeColor:[UIColor redColor].CGColor];
+    self.cubicCurveLayer = [self createShapeLayerWithStrokeColor:[UIColor greenColor].CGColor];
+}
+
+- (CAShapeLayer *)createShapeLayerWithStrokeColor:(CGColorRef)color {
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    shapeLayer.strokeColor = color;
+    shapeLayer.lineWidth = 2.0;
+    [self.view.layer addSublayer:shapeLayer];
+    return shapeLayer;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self animatePath];
-    
+    [self animatePaths];
     [self.iconFlowView playAnimationWithCount:5];
-
 }
 
-- (void)animatePath {
-    // 根据用户的选择绘制不同类型的曲线
+- (void)animatePaths {
     [self drawQuadCurve];
     [self drawCubicCurve];
-    [self drawComplexCubicCurve];
 }
 
 - (void)drawQuadCurve {
-    [self.path removeAllPoints];
-    [self.path moveToPoint:CGPointMake(50, 200)];
-    [self.path addQuadCurveToPoint:CGPointMake(300, 200) controlPoint:CGPointMake(175, 100)];
-    [self updateShapeLayer];
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(50, 200)];
+    [path addQuadCurveToPoint:CGPointMake(300, 200) controlPoint:CGPointMake(175, 100)];
+    self.quadCurveLayer.path = path.CGPath;
+    [self addAnimationToLayer:self.quadCurveLayer];
 }
 
 - (void)drawCubicCurve {
-    [self.path removeAllPoints];
-    [self.path moveToPoint:CGPointMake(50, 300)];
-    [self.path addCurveToPoint:CGPointMake(300, 300) controlPoint1:CGPointMake(100, 200) controlPoint2:CGPointMake(200, 400)];
-    [self updateShapeLayer];
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(50, 300)];
+    [path addCurveToPoint:CGPointMake(300, 300) controlPoint1:CGPointMake(100, 200) controlPoint2:CGPointMake(200, 400)];
+    self.cubicCurveLayer.path = path.CGPath;
+    [self addAnimationToLayer:self.cubicCurveLayer];
 }
 
-- (void)drawComplexCubicCurve {
-    [self.path removeAllPoints];
-    [self.path moveToPoint:CGPointMake(50, 400)];
-    [self.path addCurveToPoint:CGPointMake(300, 400) controlPoint1:CGPointMake(100, 300) controlPoint2:CGPointMake(200, 500)];
-    [self updateShapeLayer];
-}
-
-- (void)updateShapeLayer {
-    self.shapeLayer.path = self.path.CGPath;
+- (void)addAnimationToLayer:(CAShapeLayer *)layer {
     CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     pathAnimation.duration = 2.0;
-    pathAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
-    pathAnimation.toValue = [NSNumber numberWithFloat:1.0f];
-    [self.shapeLayer addAnimation:pathAnimation forKey:@"strokeEndAnimation"];
+    pathAnimation.fromValue = @0.0f;
+    pathAnimation.toValue = @1.0f;
+    [layer addAnimation:pathAnimation forKey:@"strokeEndAnimation"];
 }
 
 @end
